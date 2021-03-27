a190e5cc8098d5461c52b61655680c2f43b3cb69
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2021-03-27 14:53:47 +0530

    Added support for calculating freed storage space

--- main.mjs
+++ main.mjs
@@ -11,7 +11,7 @@ import path from 'path';
 // TODO Add support for removing any directory or file. Done.
 // TODO Add support for removing multiple directories or files. Done.
 // TODO Add list of file/directory names to exclude from search. Done.
-// TODO Add summary after finishing.
+// TODO Ouput storage space freed. Done.
 
 const rl = readLine.createInterface({
   input: process.stdin,
@@ -33,6 +33,8 @@ function parseArgs(args) {
           config.recurse = true;
         } else if (char === 'y') {
           config.skipConfirmation = true;
+        } else if (char === 's') {
+          config.getSize = true;
         } else if (char === 'd') {
           const nextArg = args[i + 1];
           if (nextArg && nextArg[0] !== '-') {
@@ -63,31 +65,87 @@ function parseArgs(args) {
     throw new Error('Target file/directory not provided.')
   }
 
+  config.targets.forEach(config.excludeList.add, config.excludeList);
+
   return config;
+}
+
+async function getAllFilesPath(dirPath) {
+  const paths = [dirPath];
+  const filePaths = [];
+
+  if ((await fs.stat(dirPath)).isFile()) {
+    return [dirPath];
+  }
+
+  for (let i = 0; i < paths.length; i++) {
+    const directoryList = await fs.opendir(paths[i]);
+
+    for await (const dirent of directoryList) {
+      const fullPath = path.join(paths[i], dirent.name);
+
+      if (dirent.isFile()) {
+        filePaths.push(fullPath);
+      } else {
+        paths.push(fullPath);
+      }
+    }
+  }
+
+  return filePaths;
+}
+
+async function getDirSize(dirPath) {
+  let totalSize = 0;
+  const filePaths = await getAllFilesPath(dirPath);
+
+  for (const filePath of filePaths) {
+    totalSize += (await fs.stat(filePath)).size;
+  }
+
+  return totalSize;
+}
+
+function convertBytes(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+  if (bytes == 0) {
+    return "0 Bytes";
+  }
+
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
 }
 
 async function removeTargetObject(path) {
   try {
     await fs.rm(path, { recursive: true });
     console.log(`File/Directory ${path} deleted successfully.`);
+    return true;
   } catch (e) {
     console.error(`Failed to delete file/directory ${path}. Reason ${e}`);
+    return false;
   }
 }
 
-function handleTargetRemoval(config, path) {
-  if (config.skipConfirmation) {
-    return removeTargetObject(path);
+async function handleTargetRemoval(getSize, skipConfirmation, path) {
+  const operation = {};
+
+  if (skipConfirmation) {
+    operation.size = getSize ? await getDirSize(path) : 0;
+    operation.isSuccess = await removeTargetObject(path);
+    return operation;
   }
 
   return new Promise((resolve) => {
     rl.question(`Remove file/directory ${path} [y/n]? `, async (input) => {
       if (input === 'y') {
-        await removeTargetObject(path);
+        operation.size = getSize ? await getDirSize(path) : 0;
+        operation.isSuccess = await removeTargetObject(path);
       } else {
         console.log(`Skipping file/directory ${path}`);
       }
-      resolve();
+      resolve(operation);
     });
   });
 }
@@ -96,7 +154,10 @@ async function main() {
   try {
     const config = parseArgs(process.argv.slice(2));
     const paths = [config.path];
-    const targetPaths = [];
+    const targetObjects = [];
+    let totalSize = 0;
+
+    console.log('Searching for files/directories...');
 
     for (let i = 0; i < paths.length; i++) {
       const directoryList = await fs.opendir(paths[i]);
@@ -105,26 +166,32 @@ async function main() {
         const fullPath = path.join(paths[i], dirent.name);
 
         if (config.targets.has(dirent.name)) {
-          targetPaths.push(fullPath);
+          targetObjects.push({ path: fullPath });
         }
 
         if (dirent.isDirectory()
           && config.recurse
-          && !config.targets.has(dirent.name)
           && !config.excludeList.has(dirent.name)) {
           paths.push(fullPath);
         }
       }
     }
 
-    if (targetPaths.length) {
-      console.log(`${targetPaths.length} target(s) found.`);
+    if (targetObjects.length) {
+      console.log(`${targetObjects.length} target(s) found.`);
     } else {
       console.log('Target file/directory not found.');
     }
 
-    for (const targetPath of targetPaths) {
-      await handleTargetRemoval(config, targetPath);
+    for (const targetObject of targetObjects) {
+      const operation = await handleTargetRemoval(
+        config.getSize, config.skipConfirmation, targetObject.path
+      );
+      totalSize += operation.size ? operation.size : 0;
+    }
+
+    if (config.getSize) {
+      console.log(`${convertBytes(totalSize)} freed.`)
     }
   } catch (err) {
     console.error(err);
