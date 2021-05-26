c12cb1c875ef33a424d2238de160f7247ec0ba4f
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2021-05-27 03:45:00 +0530

    Improved error handling

---
+++ src/request.rs
@@ -0,0 +1,91 @@
+use std::collections::HashSet;
+use std::io::{self, Write};
+use std::path::{Path, PathBuf};
+
+use clap::{ArgMatches, Values};
+use anyhow::Error;
+use fehler::throws;
+
+use crate::utils::{self, PathFilterOption};
+
+#[derive(Debug)]
+pub struct Request<'a> {
+    path: PathBuf,
+    targets: HashSet<&'a str>,
+    exclude: HashSet<&'a str>,
+    recurse: bool,
+    skip_confirmation: bool,
+    get_size: bool,
+}
+
+impl<'a> Request<'a> {
+    pub fn new(matches: &'a ArgMatches) -> Request<'a> {
+        let path = PathBuf::from(matches.value_of("directory").unwrap());
+        let targets = matches.values_of("targets").unwrap().collect();
+        let exclude = matches.values_of("exclude_directories").unwrap_or(Values::default()).collect();
+        let recurse = matches.is_present("recurse");
+        let skip_confirmation = matches.is_present("skip_confirmation");
+        let get_size = matches.is_present("size");
+
+        Request {
+            path,
+            targets,
+            exclude,
+            recurse,
+            skip_confirmation,
+            get_size,
+        }
+    }
+
+    #[throws(Error)]
+    pub fn handle(&self) {
+        let mut total_size = 0;
+        let paths_to_remove = self.get_paths_to_remove()?;
+
+        for path in paths_to_remove {
+            if self.get_confirmation(&path)? {
+                if self.get_size {
+                    total_size += utils::get_size(&path)?
+                }
+                utils::remove_path(&path)?;
+            } else {
+                println!("Skipping file/directory {:?}", &path);
+            }
+        }
+
+        if self.get_size {
+            println!("{} freed.", utils::convert_bytes(total_size as f64))
+        }
+    }
+
+    #[throws(Error)]
+    fn get_paths_to_remove(&self) -> Vec<PathBuf> {
+        utils::filter_paths(&self.path, |path| {
+            let name = path.file_name().unwrap().to_str().unwrap();
+
+            if self.targets.contains(name) {
+                PathFilterOption::Append
+            } else if path.is_dir() && self.recurse && !self.exclude.contains(name) {
+                PathFilterOption::Scan
+            } else {
+                PathFilterOption::Ignore
+            }
+        })?
+    }
+
+    #[throws(Error)]
+    fn get_confirmation(&self, path: &Path) -> bool {
+        if self.skip_confirmation {
+            true
+        } else {
+            let mut stdout = io::stdout();
+            write!(&mut stdout, "Delete file/directory {:?} [y/n]? ", &path)?;
+            stdout.flush()?;
+
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+
+            input.trim().to_lowercase() == "y"
+        }
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,84 @@
+use std::f64;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use anyhow::Error;
+use fehler::throws;
+
+pub enum PathFilterOption {
+    Append,
+    Scan,
+    Ignore,
+}
+
+#[throws(Error)]
+pub fn filter_paths<F>(path: &Path, callback: F) -> Vec<PathBuf>
+where
+    F: Fn(&Path) -> PathFilterOption,
+{
+    let mut filtered_paths: Vec<PathBuf> = Vec::new();
+    let mut dirs: Vec<PathBuf> = vec![path.to_path_buf()];
+
+    while let Some(dir) = dirs.get(0) {
+        for path in get_dir_contents(dir)? {
+            match callback(&path) {
+                PathFilterOption::Append => filtered_paths.push(path),
+                PathFilterOption::Scan => dirs.push(path),
+                PathFilterOption::Ignore => (),
+            }
+        }
+
+        dirs.remove(0);
+    }
+
+    filtered_paths
+}
+
+#[throws(Error)]
+pub fn remove_path(path: &Path) {
+    if path.is_file() {
+        fs::remove_file(&path)?;
+    } else if path.is_dir() {
+        fs::remove_dir_all(&path)?;
+    }
+}
+
+#[throws(Error)]
+pub fn get_all_files(path: &Path) -> Vec<PathBuf> {
+    if path.is_file() {
+        vec![path.to_path_buf()]
+    } else {
+        filter_paths(path, |path| -> PathFilterOption {
+            if path.is_dir() {
+                PathFilterOption::Scan
+            } else {
+                PathFilterOption::Append
+            }
+        })?
+    }
+}
+
+#[throws(Error)]
+pub fn get_size(path: &Path) -> u64 {
+    get_all_files(path)?
+        .iter()
+        .fold(0, |acc, path| acc + fs::metadata(path).unwrap().len())
+}
+
+pub fn convert_bytes(bytes: f64) -> String {
+    const SIZES: [&str; 5] = ["Bytes", "KB", "MB", "GB", "TB"];
+
+    if bytes == 0f64 {
+        "0 Bytes".to_owned()
+    } else {
+        let num = bytes.log(f64::consts::E) / 1024f64.log(f64::consts::E);
+        let i = num.trunc() as i32;
+        format!("{:.1} {}", (bytes / 1024f64.powi(i)), SIZES[i as usize])
+    }
+}
+
+#[throws(Error)]
+fn get_dir_contents(path: &Path) -> Vec<PathBuf> {
+    let entries = path.read_dir()?;
+    entries.map(|entry| entry.unwrap().path()).collect()
+}
